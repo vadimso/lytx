79d3e173d29a46100d539aff019cf6eddac61958
Vadim Sohin , DevOps  Engineer <vadim.sohin@gmail.com>
2023-10-15 10:51:04 +0300

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -3,7 +3,8 @@ resource "aws_security_group_rule" "company1_to_company2" {
   from_port   = 0
   to_port     = 65535
   protocol    = "tcp"
-  cidr_blocks = [aws_instance.company2_instance.private_ip]
+  #cidr_blocks = [aws_instance.company2_instance.private_ip]
+  cidr_blocks = ["172.31.28.39/32"]
   security_group_id = aws_security_group.ec2_sg.id
 }
 
@@ -12,6 +13,7 @@ resource "aws_security_group_rule" "company2_to_company1" {
   from_port   = 0
   to_port     = 65535
   protocol    = "tcp"
-  cidr_blocks = [aws_instance.company1_instance.private_ip]
+  #cidr_blocks = [aws_instance.company1_instance.private_ip]
+  cidr_blocks = ["172.31.16.115/32"]
   security_group_id = aws_security_group.ec2_sg.id
 }
